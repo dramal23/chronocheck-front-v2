cbfe931fdcb732792cf76f3cee261b5dd5cd9c9e
dramal23 <alexandre.mrad@dauphine.eu>
2024-04-22 12:59:42 +0200

    added logos folders and changed property of props inside MarqueItem.jsx

--- src/components/MarqueItem.jsx
+++ src/components/MarqueItem.jsx
@@ -8,7 +8,7 @@ function MarqueItem({ brand }) {
             <div className='rounded-div p-7 hover:scale-105 ease-out duration-300'>
                 <div className='flex justify-between items-center'>
                     <div>
-                        <img className='rounded-full' src={brand.img_small} alt={brand.brand_name + " Logo"}/>
+                        <img className='rounded-full' src={brand.logo_url} alt={brand.brand_name + " Logo"}/>
                         <h2 className='font-semibold text-xl'>{brand.brand_name}</h2>
                     </div>
                 </div>
